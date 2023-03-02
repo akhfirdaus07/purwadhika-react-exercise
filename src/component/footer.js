6a977573090ab3c2b95bb80161f6bcc2c3f64d90
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-02 22:36:34 +0700

    Install fg-loadcss and add footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,11 +1,30 @@
-import { Box } from "@mui/material"
+import * as React from 'react';
+import { loadCSS } from 'fg-loadcss';
+import Box from '@mui/material/Box';
+import { green } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
 
-export default function Footer(){
-    return(
-        <Box sx={{
-            gridRow:'3/4',
-            bgcolor: '#ccd5ae',
-            }}>
-        </Box>
-    )
+export default function Footer() {
+  React.useEffect(() => {
+    const node = loadCSS(
+      'https://kit.fontawesome.com/3f64b12415.css',
+      // Inject before JSS
+      document.querySelector('#font-awesome-css') || document.head.firstChild,
+    );
+
+    return () => {
+      node.parentNode.removeChild(node);
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        gridRow:'3/4',
+        bgcolor: '#ccd5ae',
+      }}
+    >
+      <Icon className="fa-brands fa-github" />
+    </Box>
+  );
 }
