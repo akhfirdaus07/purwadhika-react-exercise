a9c08c95d7e145e69b5712c9f1334e06bf05fa2e
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-04 15:10:04 +0700

    Add and style exercise 1

--- src/component/exercise1.js
+++ src/component/exercise1.js
@@ -1,7 +1,34 @@
+import React, {useState} from 'react';
 import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 export default function Exercise1(){
-    <Box sx={{gridRow:'2/3'}}>
-
-    </Box>
+    // const [count, setCount]=useState(0);
+    return(
+        <Box sx={{
+            gridRow:'2/3',
+            display:'flex', 
+            flexDirection:'column',
+            justifyContent:'center',
+            alignItems:'center',
+        }}>
+            <Typography variant="h1">0</Typography>
+                <Box sx={{
+                gridRow:'2/3',
+                display:'flex', 
+                gap:1, marginTop:5,
+            }}>
+                    <Button color="info" variant="contained" size="large">
+                        Increase
+                    </Button>
+                    <Button color="warning" variant="contained" size="large" >
+                        Decrease
+                    </Button>
+                    <Button color="error" variant="contained" size="large">
+                        Reset
+                    </Button>
+            </Box>
+        </Box>
+    )
 }
--- src/component/header.js
+++ src/component/header.js
@@ -1,5 +1,7 @@
-import { Box } from "@mui/system"
-import { Button } from "@mui/material"
+import { Box } from "@mui/system";
+import { Button } from "@mui/material";
+import { Typography } from '@mui/material';
+
 
 export default function Header(){
     return(
@@ -7,20 +9,24 @@ export default function Header(){
             sx={{
             gridRow:'1/2',
             display: 'flex',
-            justifyContent: 'center',
+            justifyContent: 'space-between',
+            alignItems:'center',
             bgcolor: '#ccd5ae',
             p:1, gap:5,
             }}
         >
-            <Button color="error" variant="outlined" href="purwadhika-react-exercise" size="large">
-                Exercise 1
-            </Button>
-            <Button color="error" variant="outlined" href="exercise-2" size="large" >
-                Exercise 2
-            </Button>
-            <Button color="error" variant="outlined" href="exercise-3" size="large">
-                Exercise 3
-            </Button>
+            <Typography variant="h5">Purwadhika Module 2 Slide 7 Exercise</Typography>
+            <Box sx={{display:'flex', gap:1}}>
+                <Button color="error" variant="outlined" href="purwadhika-react-exercise" size="large">
+                    Exercise 1
+                </Button>
+                <Button color="error" variant="outlined" href="exercise-2" size="large" >
+                    Exercise 2
+                </Button>
+                <Button color="error" variant="outlined" href="exercise-3" size="large">
+                    Exercise 3
+                </Button>
+            </Box>
         </Box>
     )
 }
