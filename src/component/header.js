80d677083948395b56f999b9781a735d2bd99f68
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-04 23:58:57 +0700

    Fix bug in Link router dom at header

--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/system";
-import { Button } from "@mui/material";
 import { Typography } from '@mui/material';
+import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
@@ -11,20 +11,14 @@ export default function Header(){
             justifyContent: 'space-between',
             alignItems:'center',
             bgcolor: '#ccd5ae',
-            p:1, gap:5,
+            p:5, gap:5, 
             }}
         >
             <Typography variant="h5">Purwadhika Module 2 Slide 7 Exercise</Typography>
-            <Box sx={{display:'flex', gap:1}}>
-                <Button color="error" variant="outlined" size="large" href='/purwadhika-react-exercise/exercise-1'>
-                    Exercise 1
-                </Button>
-                <Button color="error" variant="outlined" size="large" href='/purwadhika-react-exercise/exercise-2'>
-                    Exercise 2
-                </Button>
-                <Button color="error" variant="outlined" size="large" href='/purwadhika-react-exercise/exercise-3'>
-                    Exercise 3
-                </Button>
+            <Box sx={{display:'flex', alignItems:'center', justifyContent:'center', gap:5}}>
+                <Link to='/exercise-1'>Exercise 1</Link>
+                <Link to='/exercise-2'>Exercise 2</Link>
+                <Link to='/exercise-3'>Exercise 3</Link>
             </Box>
         </Box>
     )
