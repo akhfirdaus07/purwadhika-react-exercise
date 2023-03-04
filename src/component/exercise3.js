083d15c388721eab647fc7713d96995368990901
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-04 22:51:03 +0700

    Style the exercise 3

--- src/component/exercise3.js
+++ src/component/exercise3.js
@@ -1,47 +1,58 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
+import { TextField } from '@mui/material';
 
 export default function Exercise3(){
-    const [count, setCount]=useState(0);
+    useEffect(()=>{document.title='React Exercise 3'})
 
     const styles={
         paperContainer:{
             backgroundImage: `linear-gradient(to bottom right, orange, red, purple)`
         }
-    }
+    };
+    
     return(
-        <Box style={styles.paperContainer} sx={{
-            bgcolor:'#d4a373',
-            margin:15,
-            marginLeft:60,
-            marginRight:60,
-            borderRadius:10,
-            gridRow:'2/3',
-            display:'flex', 
-            flexDirection:'column',
-            justifyContent:'center',
-            alignItems:'center',
+        <Box sx={{
+            gridRow:'2/3',display:'flex', gap:5,m:8,
+            flexDirection:'column', alignItems:'center'
         }}>
-            <Typography variant="h1">{count}</Typography>
+            <Typography variant="h3">Create a filter live search</Typography>
+            <Box style={styles.paperContainer} sx={{
+                display:'flex', 
+                flexDirection:'column',
+                justifyContent:'center',
+                alignItems:'center',
+                gap:3,borderRadius:5,p:3,
+            }}>
                 <Box sx={{    
                     gridRow:'2/3',
                     display:'flex', 
-                    gap:1, marginTop:4,
+                    gap:3,
+                    justifyContent:'center',
+                    alignItems:'center',
                 }}>
-                    <Button color="info" variant="contained" size="small" onClick={()=>{setCount(count+1)}}>
-                        Increase
-                    </Button>
-                    <Button color="warning" variant="contained" size="small" onClick={()=>{setCount(count-1)}}>
-                        Decrease
-                    </Button>
-                    <Button color="error" variant="contained" size="small" onClick={()=>{setCount(count=0)}}>
-                        Reset
-                    </Button>
+                    <Typography variant="h6">Search</Typography>
+                    <TextField id="outlined-basic" label="Keyword" variant="outlined"/>
+                </Box>
+                
+                <Box sx={{    
+                    gridRow:'2/3',
+                    display:'grid', 
+                    gridTemplateColumns: "1fr 1fr",
+                    gap:3, p:1,
+                }}>
+                    <Button color="success" variant="contained">Banana</Button>
+                    <Button color="success" variant="contained">Apple</Button>
+                    <Button color="success" variant="contained">Orange</Button>
+                    <Button color="success" variant="contained">Mango</Button>
+                    <Button color="success" variant="contained">Pineapple</Button>
+                    <Button color="success" variant="contained">Watermelon</Button>
+                    <Button color="success" variant="contained">Strawberry</Button>
+                    <Button color="success" variant="contained">Persimmon</Button>
+                </Box>
             </Box>
         </Box>
     )
 }
-
-// Banana Apple Orange Mango Pineapple Watermelon Strawberry Persimmon
