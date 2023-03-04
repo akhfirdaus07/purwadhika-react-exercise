a07f5b72f0d5ea85577a8e8158fe27acc1d08964
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-03-04 15:21:48 +0700

    Add hook in exercise 1

--- src/component/exercise1.js
+++ src/component/exercise1.js
@@ -4,28 +4,33 @@ import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
 
 export default function Exercise1(){
-    // const [count, setCount]=useState(0);
+    let [count, setCount]=useState(0);
     return(
         <Box sx={{
+            bgcolor:'#d4a373',
+            margin:15,
+            marginLeft:60,
+            marginRight:60,
+            borderRadius:10,
             gridRow:'2/3',
             display:'flex', 
             flexDirection:'column',
             justifyContent:'center',
             alignItems:'center',
         }}>
-            <Typography variant="h1">0</Typography>
-                <Box sx={{
-                gridRow:'2/3',
-                display:'flex', 
-                gap:1, marginTop:5,
-            }}>
-                    <Button color="info" variant="contained" size="large">
+            <Typography variant="h1">{count}</Typography>
+                <Box sx={{    
+                    gridRow:'2/3',
+                    display:'flex', 
+                    gap:1, marginTop:4,
+                }}>
+                    <Button color="info" variant="contained" size="small" onClick={()=>{setCount(count+1)}}>
                         Increase
                     </Button>
-                    <Button color="warning" variant="contained" size="large" >
+                    <Button color="warning" variant="contained" size="small" onClick={()=>{setCount(count-1)}}>
                         Decrease
                     </Button>
-                    <Button color="error" variant="contained" size="large">
+                    <Button color="error" variant="contained" size="small" onClick={()=>{setCount(count=0)}}>
                         Reset
                     </Button>
             </Box>
